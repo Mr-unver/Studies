279021b0657e79ab4e7254ce804c39e7b06dc2eb
Mr-unver <71456743+Mr-unver@users.noreply.github.com>
2022-10-05 20:25:13 +0300

    Add files via upload

--- JS простым языком/app.js
+++ JS простым языком/app.js
@@ -1,11 +1,38 @@
-// Closures
+// promise
 
-// function create (n) {
-//     return function() {
-//     console.log(1000*n)
-//     }   
-// }
+// console.log('Request data...');
 
-// const calc = create(42)
-// calc()
+// setTimeout(() => {
+//     console.log('Preparing data...')
 
+//     const backendData = {
+//         server: 'aws',
+//         port: 2000,
+//         status: 'working'
+//     }
+
+//     setTimeout(() => {
+//         backendData.modified = true
+//         console.log('Data received', backendData)
+//     }, 2000)
+// }, 2000)
+
+const p = new Promise(function(resolve, reject) {
+    setTimeout(() =>{
+        console.log('Preparing data...')
+        const backendData = {
+            server: 'aws',
+            port: 2000,
+            status: 'working'
+        }
+        resolve(backendData)
+    }, 2000)
+})
+
+p.then(data =>{
+    const [2 = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            data.modified = true
+        })
+    })]
+})
